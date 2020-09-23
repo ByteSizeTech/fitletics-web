a10bb18e3d1b57cc0b4fcecbba392de4fcf80d2c
Nimra Gauhar <53583132+NimraGJay@users.noreply.github.com>
2020-09-23 15:12:43 +0500

    changed the webcam video acquisition code

--- src/Scripts/Session.js
+++ src/Scripts/Session.js
@@ -1,51 +1,41 @@
 function startTimer(duration, display) {
-    var timer = duration,
-        minutes, seconds, milliseconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-        milliseconds = parseInt(seconds / 1000);
+  var timer = duration,
+    minutes,
+    seconds,
+    milliseconds;
+  setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+    milliseconds = parseInt(seconds / 1000);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        milliseconds = milliseconds < 10 ? "0" + milliseconds : milliseconds;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    milliseconds = milliseconds < 10 ? "0" + milliseconds : milliseconds;
 
-        display.textContent = minutes + ":" + seconds + "." + milliseconds;
+    display.textContent = minutes + ":" + seconds + "." + milliseconds;
 
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000);
 }
 
 window.onload = function () {
-    var thirtyMinutes = 60 * 30,
-        display = document.querySelector('#time');
-    startTimer(thirtyMinutes, display);
+  var thirtyMinutes = 60 * 30,
+    display = document.querySelector("#time");
+  startTimer(thirtyMinutes, display);
 };
 
 // On this codelab, you will be streaming only video (video: true).
-const mediaStreamConstraints = {
-    video: true,
-};
+var video = document.querySelector("#session-video");
 
-// Video element where stream will be placed.
-const localVideo = document.querySelector('video');
-
-// Local stream that will be reproduced on the video.
-let localStream;
-
-// Handles success by adding the MediaStream to the video element.
-function gotLocalMediaStream(mediaStream) {
-    localStream = mediaStream;
-    localVideo.srcObject = mediaStream;
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
 }
-
-// Handles error by logging a message to the console with the error message.
-function handleLocalMediaStreamError(error) {
-    console.log('navigator.getUserMedia error: ', error);
-}
-
-// Initializes media stream.
-navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
-    .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
