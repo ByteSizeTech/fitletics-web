0d6e551f946ac5e2edaa2639192697fd493d3135
kuhr827 <kuhr827@uowmail.edu.au>
2020-10-02 19:21:42 +0400

    fixed the space issue

---
+++ src/Scripts/Session Description.js
@@ -0,0 +1,27 @@
+function startTimer(duration, display) {
+    var timer = duration,
+        minutes,
+        seconds,
+        milliseconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+        milliseconds = parseInt(seconds / 1000);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        milliseconds = milliseconds < 10 ? "0" + milliseconds : milliseconds;
+
+        display.textContent = minutes + ":" + seconds + "." + milliseconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var thirtyMinutes = 60 * 30,
+        display = document.querySelector("#time");
+    startTimer(thirtyMinutes, display);
+};
