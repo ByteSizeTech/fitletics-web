5b6d9c07ba896734f87fbf63a3669097d9ad0c0d
Nimra Gauhar <53583132+NimraGJay@users.noreply.github.com>
2020-12-02 20:34:06 +0500

    marked used and unused

--- src/Scripts/ct-posenet.js
+++ src/Scripts/ct-posenet.js
@@ -23,19 +23,18 @@ function setup() {
     // flipHorizontal: false,
     minConfidence: 0.5,
     maxPoseDetections: 1,
-    minPartConfidence:0.5,
+    minPartConfidence: 0.5,
     scoreThreshold: 0.5,
     nmsRadius: 20,
     detectionType: "single",
     inputResolution: 256,
     multiplier: 0.75,
     quantBytes: 2,
-    
 
     // // imageScaleFactor: 0.3,
     // // minConfidence: 0.5,
   };
-  poseNet = ml5.poseNet(video, posenetOpts,modelLoaded);
+  poseNet = ml5.poseNet(video, posenetOpts, modelLoaded);
   poseNet.on("pose", gotResults);
   video.hide();
 
@@ -59,19 +58,13 @@ function draw() {
 }
 
 function gotResults(results) {
-<<<<<<< HEAD
-=======
-
->>>>>>> master
   console.log(results);
 
   if (results.length > 0) {
     // console.log("result.length > than 0");
-  
-        pose = results[0].pose;
-        skeleton = results[0].skeleton;
-      
-    
+
+    pose = results[0].pose;
+    skeleton = results[0].skeleton;
   }
   console.log(pose);
 }
@@ -84,7 +77,7 @@ function drawKeypoints() {
       let keypoint = pose.keypoints[j];
       // Only draw an ellipse is the pose probability is bigger than 0.2
       if (keypoint.score > 0.2) {
-        fill(255,0,0);
+        fill(255, 0, 0);
         noStroke();
         ellipse(keypoint.position.x, keypoint.position.y, 15, 15);
       }
@@ -98,7 +91,7 @@ function drawSkeleton() {
       let partA = skeleton[j][0];
       let partB = skeleton[j][1];
       strokeWeight(2);
-      stroke(0,255,0);
+      stroke(0, 255, 0);
       line(
         partA.position.x,
         partA.position.y,
